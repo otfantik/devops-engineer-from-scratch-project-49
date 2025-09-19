03789f4c2962d038eeec9a1894dc31c6228e3ea9
otfanik <majormazov@gmail.com>
2025-09-19 17:18:02 +0300

    linter 80

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,7 +9,10 @@ def run_game(game_module):
         print(f"Question: {question}")
         user_answer = input("Your answer: ").strip()
         if user_answer != correct_answer:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(
+	        f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+                 )
             print(f"Let's try again, {name}!")
             return
         print("Correct!")
