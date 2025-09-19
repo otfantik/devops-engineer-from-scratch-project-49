8ea46867ea43f8d4b500679f403ffa27b495198d
otfanik <majormazov@gmail.com>
2025-09-19 16:18:06 +0300

    repare even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,9 @@
+import random
+GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+def is_even(number):
+    return number % 2 == 0
+def generate_round():
+    number = random.randint(1, 100)
+    correct_answer = "yes" if is_even(number) else "no"
+    question = str(number)
+    return question, correct_answer
